006f6bcb932aada17027b3bfe655d981ece58553
ogalthouse25 <owenalthouse@gmail.com>
2025-11-19 20:18:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_mssql_server" "sql" {
-  name                         = "sql-dsba6190-ogalt-dev-002"
+  name                         = "sql-dsba6190-ogalt-dev-003"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
