a09c4af2163ecea0bff7c13c80b5b9c2d43531a1
ogalthouse25 <owenalthouse@gmail.com>
2025-11-19 19:52:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,3 +42,68 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dsba6190-ogalt-dev-eastus-001"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-dsba6190-ogalt-dev-eastus-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+resource "azurerm_storage_account" "example" {
+  name                = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sql-dsba6190-ogalt-dev-001"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name         = "db-dsba6190-ogalt-dev-001"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_mssql_virtual_network_rule" "vnr" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.sql.id
+  subnet_id = azurerm_subnet.subnet.id
+}
