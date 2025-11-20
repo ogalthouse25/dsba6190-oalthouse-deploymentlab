749bc3ed54e9a1b4e7fc25757e4eac372ddab03f
ogalthouse25 <owenalthouse@gmail.com>
2025-11-19 20:08:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_mssql_server" "sql" {
-  name                         = "sql-dsba6190-ogalt-dev-001"
+  name                         = "sql-dsba6190-ogalt-dev-002"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
