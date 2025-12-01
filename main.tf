c7846366a2d752f1df9eb6013f7f1e595d688ea5
ogalthouse25 <owenalthouse@gmail.com>
2025-12-01 11:46:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,17 +31,6 @@ resource "azurerm_resource_group" "rg" {
 }
 
 
-// Storage Account
-
-resource "azurerm_storage_account" "storage" {
-  name                     = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = local.tags
-}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-dsba6190-ogalt-dev-eastus-001"
@@ -88,4 +77,23 @@ resource "azurerm_mssql_virtual_network_rule" "vn" {
   name      = "sql-vnet-rule"
   server_id = azurerm_mssql_server.sql.id
   subnet_id = azurerm_subnet.subnet.id
+}
+
+resource "azurerm_storage_account" "sto" {
+  name                = "stodsba6190ogaltdev001"
+  resource_group_name = azurerm_resource_group.rg.name
+
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
+
+  tags = {
+    environment = "staging"
+  }
 }
